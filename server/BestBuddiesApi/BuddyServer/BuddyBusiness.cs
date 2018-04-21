39a4dd67c12457baa01b706a0474c7b9c75fa1ca
Kamil <kiernoz@msn.com>
2018-04-21 14:40:35 -0400

    added db context

---
+++ server/BestBuddiesApi/BuddyServer/BuddyBusiness.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuddyServer
+{
+    public class BuddyBusiness
+    {
+        BuddyDbContext dbContext;
+
+        public BuddyBusiness()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BuddyDbContext>();
+            optionsBuilder.UseSqlServer("");
+            dbContext = new BuddyDbContext(optionsBuilder.Options);
+        }
+
+        public IEnumerable<BestBuddiesShare.Entities.Buddy> GetBuddies()
+        {
+            return dbContext.Buddies;
+        }
+    }
+}
