01899ffca5d3e2ead6636b33512bf8842cc2c188
Alice Chuang <bomberetta@gmail.com>
2018-04-21 15:11:42 -0400

    fetch txt file

--- ui/web-app/src/Header.js
+++ ui/web-app/src/Header.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
-
-
 class Header extends Component {
 	render() {
 		return (
 		  <header className="App-header">
 			  <img src="https://www.bestbuddies.org/wp-content/uploads/2017/01/best-buddies-logo2.png" className="App-logo" alt="logo" />
 			  <h1 className="App-title">Best Buddies Matching System</h1>
-			  <nav>
-				  <Link to="dashboard">Home</Link>
-			  </nav>
+			  {/*<nav>*/}
+				  {/*<Link to="dashboard">Home</Link>*/}
+			  {/*</nav>*/}
 		  </header>
 		);
 	}
